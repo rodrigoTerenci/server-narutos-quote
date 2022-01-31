32dd09d9c04e2498dc055229fb83d227448d607a
rodrigoTerenci <rodrigoterenci@gmail.com>
2022-01-31 22:46:02 +0000

    install and use csvtojson

--- exemplo.mjs
+++ exemplo.mjs
@@ -1,12 +1,46 @@
 import http from 'http'
-import { createReadStream, createWriteStream } from 'fs'
-
-//node -e 'process.stdout.write(crypto.randomBye(1e9))' > big.file
+import { readFile, readFileSync, writeFileSync, createReadStream, createWriteStream } from 'fs'
+import  csvtojson  from 'csvtojson'
 http.createServer((req, res) =>{
-    res.end('Hello')
-    createReadStream('FinalQuotes.csv')
+    let data = csvtojson()
+        .fromFile('FinalQuotes.csv')
+        .then((json) => {
+            return json
+            /*writeFileSync("output.json",JSON.stringify(json),"utf-8",(err) => {
+                if(err) console.log(err)
+            })*/
+        })
+        var r = () => Math.random() * 508
+       data.then((data)=> console.log(data[r]))
+    /* const data = readFileSync('FinalQuotes.csv', 'utf8' ,'r', (err, data) => {
+        if (err) {
+          console.error(err)
+          return
+        }
+        return JSON.stringify(data)
+      })
+      //var r = Math.random() * data.length
+      console.log(JSON.parse(data))*/
+    //res.end('Hello')
+    //createReadStream('FinalQuotes.csv')
         //.pipe(res)
 }).listen(3000, () => console.log('running at 3000'))
+
+
+/*
+import  csvtojson  from 'csvtojson'
+csvtojson()
+        .fromFile('FinalQuotes.csv')
+        .then((json) => {
+            console.log(json)
+
+            writeFileSync("output.json",JSON.stringify(json),"utf-8",(err) => {
+                if(err) console.log(err)
+            })
+        })*/
+
+//node -e 'process.stdout.write(crypto.randomBye(1e9))' > big.file
+
 
 /*
 import net from 'net'
