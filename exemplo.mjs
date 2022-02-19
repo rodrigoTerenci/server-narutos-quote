6e2cf37b183eaab0bd0e35292ad7955321076c5b
RODRIGO CARVALHO TERENCI - DIGO <45578535+rodrigoTerenci@users.noreply.github.com>
2022-02-19 12:37:26 -0400

    Update exemplo.mjs
    nossa

--- exemplo.mjs
+++ exemplo.mjs
@@ -6,7 +6,7 @@ import express from 'express'
 import cors from 'cors'
 const app = express()
 
-const port = process.env.Port || 3000
+const port = process.env.PORT || 3000
 app.use(cors({
     origin: '*'
   }))
